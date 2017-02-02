e3ba6a25a187ae642e7d3d411ff01b01bae4cd2a
Steve Bertrand <steveb@cpan.org>
2017-02-02 16:45:06 -0700

    added module struct

---
+++ lib/Github/Backup.pm
@@ -0,0 +1,111 @@
+package Github::Backup;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Github::Backup - The great new Github::Backup!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Github::Backup;
+
+    my $foo = Github::Backup->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steveb at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-github-backup at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Github-Backup>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Github::Backup
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Github-Backup>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Github-Backup>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Github-Backup>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Github-Backup/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017 Steve Bertrand.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Github::Backup
